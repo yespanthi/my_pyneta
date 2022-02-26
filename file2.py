2cb34cff13b96cbdf7da4efdc883117d41395b66
yespanthi <yespanthi@gmail.com>
2022-02-26 13:06:09 -0800

    added

---
+++ file2.py
@@ -0,0 +1,17 @@
+for i in range(100):
+     print(i)
+ 
+-print("something")
+-print("something")
+-print("something")
+-print("something")
+-print("something")
++for i in range(4):
+print("something")
+  5 -print("something")
+  6 -print("something")
+  7 -print("something")
+  8 -print("something")
+  9 +for i in range(4):
+ 10  print("something")
+ print("something")
